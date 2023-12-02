10683fdb4986b5572a56d6c3e6aee30463465993
Tiago Sabath <tiago.sabath@gmail.com>
2023-12-02 08:37:43 -0300

    Initial commit

---
+++ src/test/java/com/example/propertiesvalue/PropertiesValueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.propertiesvalue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PropertiesValueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
